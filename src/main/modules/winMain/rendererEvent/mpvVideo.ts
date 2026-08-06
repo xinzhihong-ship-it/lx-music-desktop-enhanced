027b897e6ba1fa62e933aeb900c62c9776143792
Kimi Code <kimi@example.com>
2026-08-06 18:07:05 +0800

    fix: wait for Windows mpv host window

--- src/main/modules/winMain/rendererEvent/mpvVideo.ts
+++ src/main/modules/winMain/rendererEvent/mpvVideo.ts
@@ -1,47 +1,71 @@
-import { mainHandle } from '@common/mainIpc'
-import { WIN_MAIN_RENDERER_EVENT_NAME } from '@common/ipcNames'
-import * as mpvVideo from '../mpvVideoController'
+import { mainHandle } from "@common/mainIpc";
+import { WIN_MAIN_RENDERER_EVENT_NAME } from "@common/ipcNames";
+import * as mpvVideo from "../mpvVideoController";
 
 interface VideoLoadParams {
-  videoUrl: string
-  audioUrl?: string
+	videoUrl: string;
+	audioUrl?: string;
 }
 
 export default () => {
-  mainHandle(WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_init, async() => {
-    mpvVideo.init()
-  })
-  mainHandle<VideoLoadParams>(WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_loadUrl, async({ params }) => {
-    return mpvVideo.loadUrl(params.videoUrl, params.audioUrl)
-  })
-  mainHandle(WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_play, async() => {
-    mpvVideo.play()
-  })
-  mainHandle(WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_pause, async() => {
-    mpvVideo.pause()
-  })
-  mainHandle(WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_stop, async() => {
-    mpvVideo.stop()
-  })
-  mainHandle<number>(WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_seek, async({ params }) => {
-    mpvVideo.seek(params)
-  })
-  mainHandle<number>(WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_setVolume, async({ params }) => {
-    mpvVideo.setVolume(params)
-  })
-  mainHandle<string>(WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_setAudioDevice, async({ params }) => {
-    mpvVideo.setAudioDevice(params)
-  })
-  mainHandle(WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_getPosition, async() => mpvVideo.getPosition())
-  mainHandle(WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_getDuration, async() => mpvVideo.getDuration())
-  mainHandle(WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_getPaused, async() => mpvVideo.getPaused())
-  mainHandle<Electron.Rectangle>(WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_setBounds, async({ params }) => {
-    mpvVideo.setBounds(params)
-  })
-  mainHandle<boolean>(WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_setVisible, async({ params }) => {
-    mpvVideo.setVisible(params)
-  })
-  mainHandle(WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_destroy, async() => {
-    return mpvVideo.destroy()
-  })
-}
+	mainHandle(WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_init, async () => {
+		return mpvVideo.init();
+	});
+	mainHandle<VideoLoadParams>(
+		WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_loadUrl,
+		async ({ params }) => {
+			return mpvVideo.loadUrl(params.videoUrl, params.audioUrl);
+		},
+	);
+	mainHandle(WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_play, async () => {
+		mpvVideo.play();
+	});
+	mainHandle(WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_pause, async () => {
+		mpvVideo.pause();
+	});
+	mainHandle(WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_stop, async () => {
+		mpvVideo.stop();
+	});
+	mainHandle<number>(
+		WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_seek,
+		async ({ params }) => {
+			mpvVideo.seek(params);
+		},
+	);
+	mainHandle<number>(
+		WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_setVolume,
+		async ({ params }) => {
+			mpvVideo.setVolume(params);
+		},
+	);
+	mainHandle<string>(
+		WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_setAudioDevice,
+		async ({ params }) => {
+			mpvVideo.setAudioDevice(params);
+		},
+	);
+	mainHandle(WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_getPosition, async () =>
+		mpvVideo.getPosition(),
+	);
+	mainHandle(WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_getDuration, async () =>
+		mpvVideo.getDuration(),
+	);
+	mainHandle(WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_getPaused, async () =>
+		mpvVideo.getPaused(),
+	);
+	mainHandle<Electron.Rectangle>(
+		WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_setBounds,
+		async ({ params }) => {
+			mpvVideo.setBounds(params);
+		},
+	);
+	mainHandle<boolean>(
+		WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_setVisible,
+		async ({ params }) => {
+			mpvVideo.setVisible(params);
+		},
+	);
+	mainHandle(WIN_MAIN_RENDERER_EVENT_NAME.mpv_video_destroy, async () => {
+		return mpvVideo.destroy();
+	});
+};
